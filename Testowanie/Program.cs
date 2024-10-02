b21d5d23f5786e09d9480cf981824452b2047bdf
Tomasz Kuzel <tomek.kuzel@gmail.com>
2024-10-02 12:43:58 +0200

    Add project files.

---
+++ Testowanie/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+    class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Czesc, testujemy");
+        Console.WriteLine("Podaj liczbe calkowita: ");
+
+        string input = Console.ReadLine();
+
+        int number;
+
+        bool success = int.TryParse(input, out number);
+
+        if (success)
+        {
+            Console.WriteLine("Podales liczbe: " + number);
+        }
+        else
+        {
+            Console.WriteLine("To nie jest liczba calkowita");
+        }
+    }
+}
+
+
+
