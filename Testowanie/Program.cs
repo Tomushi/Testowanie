0832f6229b181d7f85f5012cf760666776b0109c
Tomasz Kuzel <tomek.kuzel@gmail.com>
2024-10-07 16:44:52 +0200

    Testowanie int

--- Testowanie/Program.cs
+++ Testowanie/Program.cs
@@ -1,27 +1,28 @@
 ﻿using System;
-    class Program
+
+namespace InformacjeoUzytkowniku
 {
-    static void Main()
+    class Program
     {
-        Console.WriteLine("Czesc, testujemy");
-        Console.WriteLine("Podaj liczbe calkowita: ");
-
-        string input = Console.ReadLine();
-
-        int number;
-
-        bool success = int.TryParse(input, out number);
-
-        if (success)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Podales liczbe: " + number);
-        }
-        else
-        {
-            Console.WriteLine("To nie jest liczba calkowita");
+           
+            Console.WriteLine("Podaj swoje imię: ");
+            string imie = Console.ReadLine();
+
+            int a = 100;
+            int b = 200;
+            int c = a + b;
+            string potrawa = "paczki";
+            string napoj = "piwo";
+            double cena = 5.25;
+
+            Console.WriteLine("zmienna a = " + a);
+            Console.WriteLine("zmienna b = " + b);
+            Console.WriteLine("zmienna c = " + c);
+            Console.WriteLine("zmienna potrawa = " + potrawa);
+            Console.WriteLine("zmienna napoj = " + napoj);
+            Console.WriteLine("zmienna cena = " + cena);
         }
     }
 }
-
-
-
