c5a2d55c078c210b6b5b4d320bfc020e61e9c00b
Tomasz Kuzel <tomek.kuzel@gmail.com>
2024-10-07 21:54:43 +0200

    utrwalanie podstawowych schematow

--- Testowanie/Program.cs
+++ Testowanie/Program.cs
@@ -8,17 +8,49 @@ namespace InformacjeoUzytkowniku
         {
 
             Console.WriteLine("Witaj");
-            Console.WriteLine("Podaj swoje imie");
-            string imie = Console.ReadLine();
-
             
-            double czas = 15.65;
-            int cyfra = 7;
-            int wiek = 20;
+            Console.WriteLine("Podaj swoje imie");
+            
+            string imie = Console.ReadLine();
+            
+            Console.WriteLine("Witaj " + imie);
+            
+            Console.WriteLine("Podaj swoje nazwisko");
+            
+            string nazwisko = Console.ReadLine();
+            
+            Console.WriteLine("Super nazwisko " + nazwisko);
+            
+            Console.WriteLine("Podaj swoj wiek");
+            
+            int wiek = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Twój wiek to " + wiek);
+            
+            string sex = "";
+            
+            bool validSex = false;
+            
+            while (!validSex)
+            {
+                Console.WriteLine("Podaj swoja plec (M lub K)");
+            
+                sex = Console.ReadLine();
+                
+                if (sex == "M" || sex == "K")
 
-            Console.WriteLine($"Czas: {czas}");
-            Console.WriteLine($"Cyferka: {cyfra}");
-            Console.WriteLine("Wiek " + wiek);
+                    validSex = true;
+                
+                else
+                    Console.WriteLine("Podales zla plec");
+            }
+            
+            Console.WriteLine("Twoja plec " + sex);
+            
+            if (sex == "M")
+                Console.WriteLine("Czyli jestes normalnym facetem");
+            else
+                Console.WriteLine("Czyli jestes normalna kobieta");
         }
 
     }
