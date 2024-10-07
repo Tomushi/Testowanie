b2a5ede73ba258aadb9a81455cfd30f352c5ea76
Tomasz Kuzel <tomek.kuzel@gmail.com>
2024-10-07 17:21:59 +0200

    Znak dolara

--- Testowanie/Program.cs
+++ Testowanie/Program.cs
@@ -6,23 +6,18 @@ namespace InformacjeoUzytkowniku
     {
         static void Main(string[] args)
         {
-           
-            Console.WriteLine("Podaj swoje imię: ");
+
+            Console.WriteLine("Witaj");
+            Console.WriteLine("Podaj swoje imie");
             string imie = Console.ReadLine();
 
-            int a = 100;
-            int b = 200;
-            int c = a + b;
-            string potrawa = "paczki";
-            string napoj = "piwo";
-            double cena = 5.25;
+            
+            double czas = 15.65;
+            int cyfra = 7;
 
-            Console.WriteLine("zmienna a = " + a);
-            Console.WriteLine("zmienna b = " + b);
-            Console.WriteLine("zmienna c = " + c);
-            Console.WriteLine("zmienna potrawa = " + potrawa);
-            Console.WriteLine("zmienna napoj = " + napoj);
-            Console.WriteLine("zmienna cena = " + cena);
+            Console.WriteLine($"Czas: {czas}");
+            Console.WriteLine($"Cyferka: {cyfra}");
         }
+
     }
 }
