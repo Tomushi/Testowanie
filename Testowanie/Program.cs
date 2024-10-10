85ed1df9e589e9bc4b2251117c566143b417b09b
Tomasz Kuzel <tomek.kuzel@gmail.com>
2024-10-10 17:20:35 +0200

    Test bool, else, else if

--- Testowanie/Program.cs
+++ Testowanie/Program.cs
@@ -32,6 +32,46 @@ namespace Testowanie
 
             Console.WriteLine("Twoja odpowiedz to " + odp3 + " Ja pierdziele, jestem zszokowany");
 
+            string temp;
+
+            bool validTemp = false;
+
+            while (!validTemp)
+            {
+                Console.WriteLine("Czwarte pytanie, bedzie wybor");
+
+                Console.WriteLine("Czy jest zimny czy cieply? Wybierz \"C\" dla cieply lub \"Z\" dla zimny");
+
+                temp = Console.ReadLine();
+
+                if (temp == "C")
+                {
+                    Console.WriteLine("Dziekuje za odpowiedz");
+                }
+                else if (temp == "Z")
+                {
+                    Console.WriteLine("Dziekuje za odpowiedz");
+                }
+                else
+                {
+                    Console.WriteLine("Niepoprawna odpowiedz");
+                }
+
+                Console.WriteLine("Rozumiem ale pamietaj, ");
+
+                if (temp == "C")
+                
+                    Console.WriteLine("ze bedzie gorecej");
+                
+                else if (temp == "Z")
+                    
+                    Console.WriteLine("aby zalozyc czapke na wszelki wypadek");
+
+                {
+                    validTemp = true;
+                }
+
+            }
             Console.WriteLine("Dziekuje i do widzenia");
 
         }
